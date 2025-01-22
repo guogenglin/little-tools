c3cc09c97777efbd8f93278479ad3982b6310a79
Genglin Guo <108860907+guogenglin@users.noreply.github.com>
2025-01-22 09:52:45 +0800

    Add files via upload

---
+++ amr_heatmap/abricate_summary_to_0and1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 09:50:18 2025
+
+@author: 86182
+"""
+
+amr_summary = open('C:/Users/86182/Desktop/amrsummary.tab', 'rt').readlines()
+amr_file = open('C:/Users/86182/Desktop/amrs.txt', 'wt')
+amr_file.write(amr_summary[0])
+for line in amr_summary[1:]:
+    context = line.strip().split('\t')
+    amr_file.write(context[0])
+    amr_file.write('\t')
+    amr_file.write(context[1])
+    amr_file.write('\t')
+    for i in context[2:]:
+        if i == '.':
+            amr_file.write('0')
+        else:
+            amr_file.write('1')
+        amr_file.write('\t')
+    amr_file.write('\n')
+amr_file.close()
